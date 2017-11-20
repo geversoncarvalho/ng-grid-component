47b681e78ab54c323dffac46d762cada2e5f8a62
Geverson Carvalho <geverson.carvalho@outlook.com>
2017-11-20 12:37:17 -0200

    Commiting base angular 5 AOT structure

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppModuleNgFactory } from './app/app.module.ngfactory';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
